c4822456cafdf0b1aa16604cca8c2c7a19e0663b
MagtuloyRML <58243039+MagtuloyRML@users.noreply.github.com>
2022-02-13 16:06:28 +0800

    add user and user role with validation, and updated testdb

---
+++ Administrator/System User Role Maintenance/assets/addRole.php
@@ -0,0 +1,22 @@
+<?php
+    // for Connection.php
+    include_once '../../../assets/connection/DBconnection.php';
+
+    $roleNameCheck = $_POST['roleNameCheck'];
+    $roleNameCheckNew = strtoupper($roleNameCheck);
+    $studCouncheck = $_POST['studCouncheck'];
+    $studViolationcheck = $_POST['studViolationcheck'];
+    $sysMainsCheck = $_POST['sysMainsCheck'];
+
+    //insert sa database
+    $insert="INSERT INTO adminuserrole (AdminUserRole, AdminPageStudentCounceling, AdminPageViolation,
+    AdminMaintenance, StatusID) VALUES ('$roleNameCheckNew','$studCouncheck','$studViolationcheck','$sysMainsCheck','1')";
+    $query_run = mysqli_query($conn, $insert);
+    if($query_run){
+        echo "Data Inserted";
+        
+    }else{
+        echo "something is wrong" . $insert . $conn->error;
+    }
+
+?> 
