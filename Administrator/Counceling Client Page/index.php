f66cfeccb8a2e090deb042720243a6062bcd159d
brianpacheca <brianpacheca123@gmail.com>
2022-02-19 11:31:34 +0800

    Revert "big update brian w/o admin profile"
    This reverts commit 3c83bc3a42bb7457af6a9bb8182b7d3dd1e401a0.

--- Administrator/Counceling Client Page/index.php
+++ Administrator/Counceling Client Page/index.php
@@ -2,62 +2,8 @@
     $title = 'Counceling Client';
     $page = 'ca_client';
     include_once('../includes/header.php');
-
-    $id = $_SESSION['AdminID'];
-    $sql_fetch = mysqli_query($conn, "SELECT * from adminaccountinfo WHERE AdminAccountID = '$id'");
-    $name = "";
-    while($row = mysqli_fetch_assoc($sql_fetch))
-    {
-        $name = $row['AdminAccountID'];
-    }
-    include '../../assets/connection/dbconnection.php';
-    
-
-    $sched = $conn->query("SELECT 
-    `id`,
-    `ClientFirstName` as firstName,
-    `ClientMiddleName` as middleName,
-    `ClientLastName` as lastName,
-    `ClientStudentNo` as studNum,
-    `ClientAddress` as c_address,
-    `ClientContactNo` as c_contact_num,
-    `ClientGuardian` as guardian_name,
-    `ClientGuardianNo` as guardian_num,
-    `end_app`,
-    `stat`,
-    `anonymity`,
-    `AdminFirstName` as a_firstName,
-    `AdminLastName` as a_lastName
-    FROM schedules
-    INNER JOIN clientaccountinfo ON schedules.client_id = clientaccountinfo.ClientAccountID
-    INNER JOIN adminaccountinfo ON schedules.remarks = adminaccountinfo.AdminAccountID 
-    
-    WHERE `anonymity` = 'No' AND `stat` = 'Done' AND `remarks` = '$name'");
-
-$query = $conn->query("SELECT 
-`id`,
-`ClientFirstName` as firstName,
-`ClientMiddleName` as middleName,
-`ClientLastName` as lastName,
-`ClientStudentNo` as studNum,
-`ClientAddress` as c_address,
-`ClientContactNo` as c_contact_num,
-`ClientGuardian` as guardian_name,
-`ClientGuardianNo` as guardian_num,
-`end_app`,
-`stat`,
-`anonymity`,
-`AdminFirstName` as a_firstName,
-`AdminLastName` as a_lastName
-FROM schedules
-INNER JOIN clientaccountinfo ON schedules.client_id = clientaccountinfo.ClientAccountID
-INNER JOIN adminaccountinfo ON schedules.remarks = adminaccountinfo.AdminAccountID 
-
-WHERE `anonymity` = 'Yes' AND `stat` = 'Done' AND `remarks` = '$name'");
-
 ?>
-
-<div class="body_container">
+    <div class="body_container">
         <div class="content">
             <div class="client_content">
                 <div class="title">
@@ -76,69 +22,23 @@ WHERE `anonymity` = 'Yes' AND `stat` = 'Done' AND `remarks` = '$name'");
                     <h3 class="list_title">Monitored Client</h3>
                     <table class="display_client">
                         <tr> 
-                        <th class="client_title">Appointment ID</th>
                             <th class="client_title">Name</th>
-                            <th class="client_title">Student Number</th>
-                            <th class="client_title">Client Address</th>
-                            <th class="client_title">Client Contact Number</th>
-                            <th class="client_title">Guardian Name</th>
-                            <th class="client_title">Guardian Contact Number</th>
-                            <th class="client_title">Date of Appointment</th>
-                            <th class="client_title">Monitored By:</th>
-                            <th class="client_title">Status</th>
+                            <th class="client_title">Course</th>
+                            <th class="client_title">Section</th>
+                            <th class="client_title">A.Y Code</th>
+                            <th class="client_title">Violation</th>
+                            <th class="client_title">Sanction</th>
+                            <th class="client_title">Date</th>
                         </tr>
-                        <?php while($row = $sched->fetch_array()){
-                            $c_nameFormat = $row['lastName'].', '.$row['firstName'].' '.$row['middleName'];
-                            $studNum = $row['studNum'];
-                            $address = $row['c_address'];
-                            $contactNum = $row['c_contact_num'];
-                            $guardianName = $row['guardian_name'];
-                            $guardianNum = $row['guardian_num'];
-                            $dateFormat = date("d/m/Y", strtotime($row['end_app']));
-                            $status = $row['stat'];
-                            $a_nameFormat = $row['a_lastName'].', '.$row['a_firstName'];
-                            $id = $row['id'];
-                            
-                            ?>
                         <tr>
-                        <td class="client_data"><?= $id ?></td>
-                            <td class="client_data"><?= $c_nameFormat ?></td>
-                            <td class="client_data"><?= $studNum ?></td>
-                            <td class="client_data"><?= $address ?></td>
-                            <td class="client_data"><?= $contactNum ?></td>
-                            <td class="client_data"><?= $guardianName ?></td>
-                            <td class="client_data"><?= $guardianNum ?></td>
-                            <td class="client_data"><?= $dateFormat ?></td>
-                            <td class="client_data"><?= $a_nameFormat ?></td>
-                            <td class="client_data"><?= $status ?></td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
+                            <td class="client_data">data</td>
                         </tr>
-                        <?php } ?>
-                    </table>
-                </div>
-                <div class="list_client">
-                    <h3 class="list_title">Monitored Client(Anonymous)</h3>
-                    <table class="display_client">
-                        <tr> 
-                        <th class="client_title">Appointment ID</th>
-                            <th class="client_title">Date of Appointment</th>
-                            <th class="client_title">Monitored By:</th>
-                            <th class="client_title">Status</th>
-                        </tr>
-                        <?php while($row = $query->fetch_array()){
-                            
-                            $dateFormat = date("d/m/Y", strtotime($row['end_app']));
-                            $status = $row['stat'];
-                            $a_nameFormat = $row['a_lastName'].', '.$row['a_firstName'];
-                            $id = $row['id'];
-                            
-                            ?>
-                        <tr>
-                        <td class="client_data"><?= $id ?></td>
-                            <td class="client_data"><?= $dateFormat ?></td>
-                            <td class="client_data"><?= $a_nameFormat ?></td>
-                            <td class="client_data"><?= $status ?></td>
-                        </tr>
-                        <?php } ?>
                     </table>
                 </div>
 
--- Client/Client Appointment Scheduling/index.php
+++ Client/Client Appointment Scheduling/index.php
@@ -2,14 +2,6 @@
     $title = 'Appointment Scheduling';
     $page = 'client_app_sched';
     include_once('../includes/header.php');
-    
-    $id = $_SESSION['StudID'];
-    $sql_fetch = mysqli_query($conn, "SELECT * from clientaccountinfo WHERE ClientAccountID = '$id'");
-    $name = "";
-    while($row = mysqli_fetch_assoc($sql_fetch))
-    {
-        $name = $row['ClientAccountID'];
-    }
 ?>
     <div class="content">
         <div class="appoint_form">
@@ -17,7 +9,7 @@
                 <h3>Appointment Schedule</h3>
             </div>
             <div class="content_appoint">
-                <form action = "dateSelection.php" method = "POST">
+                <form action = "scheduleCheck.php" method = "POST">
                     <div class="form_content">
                         <!-- message if ever recorded or taken yung sched -->
                         <?php if (isset($_GET['error'])) { ?>
@@ -30,55 +22,52 @@
 
                         <p>Fill up the following to appoint a Sched</p>
                         <div class="input_container">
-                            <label class="label" for="#">Student Name: </label>
+                            <label class="label" for="#">Student Name(Optional): </label>
                             <br>
                             <input class="input" type="text" id="#" name="name">
-                            <input class="input" type="hidden" id="id" name="id" value="<?= $name ?>">
                         </div>
                         <div class="input_container">
-                            <label class="label" for="#">Email Address: </label>
+                            <label class="label" for="#">Email Address(Required): </label>
                             <br>
-                            <input class="input" type="text" id="#" name="email_add">
-                        </div>
-                        <div >
-                            <label class="label" for="#">Do you want to be Anonymous or Not: </label>
-                            <br>
-                            <div>
-                            <input class="input" type="radio" id="yes" name="anonymous" value="Yes" checked>
-                            <label class="label" for="Yes">Yes</label>
-                                </div>
-                            <div>
-                            <input class="input" type="radio" id="no" name="anonymous" value="No" >
-                            <label class="label" for="No">No</label>
-                            </div>
+                            <input class="input" type="text" id="#" required name="email_add">
                         </div>
                         <div class="input_container">
-                            <label class="label" for="#">What's bothering you? </label>
+                        <?php 
+                            include('../../assets/connection/dbconnection.php');
+                            $SQL = $conn->query("SELECT `meta_field`,
+                            `start_date`,
+                            `end_date` FROM `avail_sched` WHERE `meta_field` = 'first'");
+
+                                while ($row = $SQL->fetch_assoc()) {
+                            ?>
+                            <label class="label" for="#">Date of Appointment: </label>
                             <br>
-                            <input class="input" type="text" id="#" name="reason">
+                            <input class="input" type="date" required id="start_app_date" name="start_app_date" min = "<?php echo $row['start_date']; ?>" max = "<?php echo $row['end_date']; ?>">
+                            <?php
+                            }
+                          ?>
                         </div>
+                        
                         <div class="input_container">
-                            <br>
-                        <div class="input_container">
-                            <select class="label" required name = "counselor_id" id="counselor_id">
-                            <br>
-                            <option disabled value="" selected ="selected">Select a Counselor</option>
                             <?php 
-                                
-                                $query = "SELECT * from adminaccountinfo";
-                                $result1 = mysqli_query($conn, $query);
-                                while($row2 = mysqli_fetch_assoc($result1))
-                                {?>
-                                <option value="<?php echo $row2["AdminAccountID"];?>"
-                                ><?php echo $row2['AdminLastName'].', '. $row2['AdminFirstName'].' '.$row2['AdminMiddleName']; ?></option>
-                                <?php } ?>
-                                
-                            </select>
+                            include('../../assets/connection/dbconnection.php');
+                            $SQL = $conn->query("SELECT `meta_field`,
+                            `start_time`,
+                            `end_time` FROM `avail_sched` WHERE `meta_field` = 'first'");
+
+                                while ($row = $SQL->fetch_assoc()) {
+                            ?>
+                            <label class="label" for="#">Time of Appointment: </label>
+                            <br>
+                            <input class="input" type="time" required id="start_app_time" name="start_app_time" step="3600" min ="<?php echo $row['start_time'];?>" max="<?php echo $row['end_time']; ?>" >
+                            <?php
+                            }
+                        
+                          ?>
                         </div>
-                     
                     </div>
                     <div class="form_action">
-                        <button type="submit" name="submit" class="form_bttn">Next</a>
+                        <button type="submit" name="submit" class="form_bttn">Appoint</a>
                     </div>
                 
                 </form>
